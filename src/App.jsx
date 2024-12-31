b64d8487577c48e588bf32fa548df7b441b61f30
Abdurrahmaan9 <ac104442@students.cavendish.co.zm>
2024-12-31 15:13:23 +0200

    rendering pages conditionaly

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SideBar from "./components/SideBar";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
   const [showModel, setShowModel] = useState(false);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   function handleSideDisplayModel() {
     setShowModel(!showModel);
   }
 
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url =
+        "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
+
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        setData(apiData);
+        console.log("DATA\n", apiData);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchAPIData();
+  }, []);
+
   return (
     <>
-      <Main />
-      {showModel && <SideBar handleSideDisplayModel={handleSideDisplayModel} />}
-      <Footer handleSideDisplayModel={handleSideDisplayModel} />
+      {data ? (
+        <Main />
+      ) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}
+      {showModel && (
+        <SideBar data={data} handleSideDisplayModel={handleSideDisplayModel} />
+      )}
+      {data && (
+        <Footer data={data} handleSideDisplayModel={handleSideDisplayModel} />
+      )}
     </>
   );
 }
